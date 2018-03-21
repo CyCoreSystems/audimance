a884b229f5a58d493611be985cc9a03c6cec7e93
Seán C McCord <ulexus@gmail.com>
2018-03-21 13:12:51 -0400

    Add websocket reconnection

--- app/room.js
+++ app/room.js
@@ -2,92 +2,105 @@ var agenda = {}
 var roomName = ''
 var room = {}
 var tracks = {}
+var performanceTime = {}
 
-var cueChange = new Event('cueChange')
-var timeSync = new Event('timeSync')
+class PerformanceTime extends EventEmitter3 {
 
-// performanceTime tracks the server-side time coordinates
-// as they are eminated from the server.
-var performanceTime = new WebSocket("ws://"+ location.host +'/ws/performanceTime')
+   constructor() {
+      super()
 
-// cues stores the cues which have been announced by the servers, along with
-// the times of their occurrence.  This will always appear in order of receipt,
-// so the last cue will always be the last cue to have been received.
-//
-// cues are stored in the structure:
-// {
-//   cue: "intro",      // name of cue
-//   at: 1519211809934  // time at which the cue was received
-// }
-performanceTime.cues = []
+      // cues stores the cues which have been announced by the servers, along with
+      // the times of their occurrence.  This will always appear in order of receipt,
+      // so the last cue will always be the last cue to have been received.
+      //
+      // cues are stored in the structure:
+      // {
+      //   cue: "intro",      // name of cue
+      //   at: 1519211809934  // time at which the cue was received
+      // }
+      this.cues = []
 
-// sinceCue returns the number of milliseconds since the named cue.  If the cue
-// has not yet occurred, it returns a negative value.
-performanceTime.sinceCue = function(cueName) {
-   let now = Date.now()
+      this.connectWS()
 
-   var ret = -1
-
-   performanceTime.cues.forEach(function(c) {
-      if (c.cue == cueName) {
-         ret = now - c.at
-      }
-   })
-
-   return ret
-}
-
-performanceTime.addEventListener('open', function(ev) {
-   console.log("connected to performance time")
-})
-
-performanceTime.addEventListener('close', function(ev) {
-   console.log("performance time closed")
-})
-
-performanceTime.addEventListener('error', function(err) {
-   console.log("error receiving performance time: "+ err)
-})
-
-performanceTime.addEventListener('message', function(ev) {
-   var newCue = ''
-
-   console.log("received performanceTime message from server")
-   
-   // Milliseconds since UNIX epoch
-   let now = Date.now()
-
-   let t = JSON.parse(ev.data)
-
-   if (t === undefined) {
-      return
    }
 
-   if (performanceTime.cue != t.cue) {
-      newCue = t.cue
-   }
+   // sinceCue returns the number of milliseconds since the named cue.  If the cue
+   // has not yet occurred, it returns a negative value.
+   sinceCue(cueName) {
+      let now = Date.now()
 
-   var cues = []
-   t.time_points && t.time_points.forEach(function(tp) {
+      var ret = -1
 
-      cues.push({
-         cue: tp.cue,
-         at: now - (tp.offset * 1000)
+      this.cues.forEach(function(c) {
+         if (c.cue == cueName) {
+            ret = now - c.at
+         }
       })
 
-   })
-   if (cues.length > 0) {
-      performanceTime.cues = cues
+      return ret
    }
 
-   if (t.cause == "cue") {
-      performanceTime.dispatchEvent(cueChange)
+   connectWS() {
+      var self = this
 
-      console.log("received cue: "+ cues[cues.length-1].cue)
-   } else {
-      performanceTime.dispatchEvent(timeSync)
+      console.log("connecting to server")
+      var ws = new WebSocket("ws://"+ location.host +'/ws/performanceTime')
+
+      ws.addEventListener('open', function(ev) {
+         console.log("connected to server")
+      })
+
+      ws.addEventListener('close', function(ev) {
+         console.log("server connection closed")
+         setTimeout(function() {
+            self.connectWS()
+         }, 1000)
+      })
+
+      ws.addEventListener('error', function(err) {
+         console.log("error receiving from server: "+ err)
+         ws.close()
+         setTimeout(function() {
+            self.connectWS()
+         }, 1000)
+      })
+
+      ws.addEventListener('message', function(ev) {
+         console.log("received performanceTime message from server")
+         
+         // Milliseconds since UNIX epoch
+         let now = Date.now()
+
+         let t = JSON.parse(ev.data)
+
+         if (t === undefined) {
+            return
+         }
+
+         var cues = []
+         t.time_points && t.time_points.forEach(function(tp) {
+
+            cues.push({
+               cue: tp.cue,
+               at: now - (tp.offset * 1000)
+            })
+
+         })
+         if (cues.length > 0) {
+            self.cues = cues
+         }
+
+         if (t.cause == "cue") {
+            self.emit("cueChange")
+
+            console.log("received cue: "+ cues[cues.length-1].cue)
+         } else {
+            self.emit("timeSync")
+         }
+      })
    }
-})
+}
+
 
 function debugToHTML(txt) {
    var dbg = document.getElementById("debug")
@@ -111,61 +124,6 @@ Howl.prototype.seekAndPlay = function() {
    }
 }
 
-// createEnvironment sets up a new audio context and
-// binds a new Resonance Audio Scene and Room to it
-function createEnvironment() {
-   var ctx = new AudioContext()
-
-   var scene = new ResonanceAudio(ctx)
-
-   scene.output.connect(ctx.destination)
-
-   scene.setRoomProperties(
-      {
-         // Room dimensions
-         width: 100.0,
-         height: 100.0,
-         depth: 100.0
-      },
-      {
-         // Wall materials
-         left: 'transparent',
-         right: 'transparent',
-         front: 'transparent',
-         back: 'transparent',
-         down: 'transparent',
-         up: 'transparent'
-      }
-   )
-
-   return {
-      context: ctx,
-      scene: scene
-   }
-}
-
-// createSource creates a Resonance Audio source from the
-// given audio element ID at the given spatial location in the room
-function createSource(env, id, loc) {
-   console.log("creating source of id: "+id)
-
-   let el = document.getElementById(id)
-
-   let elSource = env.context.createMediaElementSource(el)
-
-   let resonanceSource = env.scene.createSource()
-
-   elSource.connect(resonanceSource.input)
-
-   resonanceSource.setPosition(loc.x, loc.y, loc.z)
-
-   return {
-      el: el,
-      elSource: elSource,
-      resonanceSource: resonanceSource,
-   }
-}
-
 // loadAgenda loads the performance Agenda and executed
 // agendaLoaded() after it is retrieved
 function loadAgenda() {
@@ -185,6 +143,11 @@ function loadAgenda() {
 // agendaLoaded is called when the program agenda has been loaded from the server.
 // It processes the agenda and sets up all of the workers.
 function agendaLoaded(agenda) {
+   document.getElementById("loadStatus").innerHTML = "loaded"
+   button.disabled = false
+}
+
+function loadAudio() {
 
    if(!document.getElementById("roomName")) {
       console.log("not in a room")
@@ -232,53 +195,17 @@ function agendaLoaded(agenda) {
          // handle the _first_ time sync to make sure we know where things lie
          // if we do not have performanceTime available before we load the
          // track
-         performanceTime.addEventListener('timeSync', function cb(ev) {
+         performanceTime.once('timeSync', function cb(ev) {
            src.seekAndPlay()
-           ev.currentTarget.removeEventListener(ev.type, cb)
          })
 
          // seek and play any time we receive a cue change
-         performanceTime.addEventListener('cueChange', function() {
+         performanceTime.on('cueChange', function() {
             src.seekAndPlay()
          })
       })
 
    })
-   
-
-   /* Resonance Audio method 
-   // Set up resonance audio
-   env = createEnvironment()
-
-   // Add sources
-   roomData.sources.forEach( function(s) {
-
-      // TODO: figure out how and if resonance audio can handle more than one
-      // track per source
-      if(!s.tracks || s.tracks.length != 1) {
-         return
-      }
-
-      let track = s.tracks[0]
-
-      var src = createSource(env, track.id, s.location)
-
-      // add the track data to the source
-      src["track"] = track
-
-      sources[s.id] = src
-
-      // seek and play to current
-      seekAndPlay(src)
-
-      // seek and play any time we receive a cue change
-      performanceTime.addEventListener('cueChange', function(ev) {
-         seekAndPlay(src)
-      })
-
-   } )
-   */
-   
 }
 
 function seekAndPlay(src) {
@@ -294,6 +221,15 @@ function seekAndPlay(src) {
 
 Howler.mobileAudioEnabled = true
 
+var button = document.getElementById("playButton")
+button.addEventListener("click", function cb(ev) {
+   loadAudio()
+   ev.currentTarget.removeEventListener(ev.type, cb)
+   button.innerHTML = "playing"
+   button.disabled = true
+})
+
+performanceTime = new PerformanceTime()
+
 window.onload = loadAgenda
 
-console.log("hello")
