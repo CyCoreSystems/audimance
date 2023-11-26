18e8be67adec07ccb836c5d057a98b89316bc915
Seán C McCord <ulexus@gmail.com>
2023-11-26 17:47:25 -0500

    comment index.js

--- example/js/index.js
+++ example/js/index.js
@@ -4,9 +4,13 @@ window.onload = function() {
    let performanceTime = new PerformanceTime()
 
    // Performance redirect
+   // This URL should be the canoncal URL to which the user should be directed when the
+   // performance is about to begin.
    var performanceURL = 'https://my.domain.com/live'
 
-   performanceTime.addEventListener('Speech', function() {
+   // ToPerformance here is the name of the cue on receipt of which, the browser should redirect
+   // the user to the live performance page.
+   performanceTime.addEventListener('ToPerformance', function() {
       window.location.href = performanceURL
    })
 }
